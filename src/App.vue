290bd3d6d18e85e812c1e003a5ad702ae396dca4
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-12-02 22:31:13 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref, watch } from "vue";
+import cranberryImage from "./assets/cranberry.png";
+import lemonImage from "./assets/lemon.svg";
+
+
+let currentLogo = ref(cranberryImage);
+let count = ref(0);
+
+watch(count,(newCount) => {
+  if(count.value>=15){
+    currentLogo.value = lemonImage
+  } else{
+    currentLogo.value = cranberryImage
+  }
+})
+
+</script>
+
+<template>
+  <div class="app">
+    <header>
+      <div class="header_logo">
+        <img class="logo" v-bind:src="currentLogo" alt="Лого" />
+        <h1>Дневник головной боли</h1>
+      </div>
+    </header>
+    <div>
+      <span>
+        Количество дней с головной болью - {{ count }}
+        <button @click="count++">Болит голова</button>
+      </span>
+      <div v-if="count >= 15" class="danger">Хроническая головная боль</div>
+    </div>
+    <footer>Natusik, 2025</footer>
+  </div>
+</template>
+
+<style scoped>
+.danger {
+  color: red;
+  font-weight: bold;
+  margin-top: 10px;
+}
+.logo {
+  width: 50px;
+  height: 50px;
+  vertical-align: middle;
+  margin-right: 10px;
+}
+.h1 {
+  font-size: 16px;
+}
+.header_logo {
+  display: flex;
+  gap: 10px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// import { createApp, ref } from 'vue'
+
+// createApp({
+//     setup() {
+//         return {
+//             count: ref(0)
+//         }
+//     }
+// }).mount('#app')
