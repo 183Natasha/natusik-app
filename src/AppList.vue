bdce260d501d6eb0e1ecbe31281a9a1639de8405
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-12-05 18:23:45 +0500

    submitForm

--- src/AppList.vue
+++ src/AppList.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref } from "vue";
 
+function submitForm() {
+  console.log("submit");
+}
 // const questionList = ref([
 //   { id: 1,
 //     text: "Сегодняшняя дата" },
@@ -39,120 +42,124 @@ import { ref } from "vue";
 
 <template>
   <div class="'container'">
-    <form class="card"></form>
-    <h2>Список вопросов</h2>
-    <ol>
-      <li>
-        <div class="form-control">
-          <label for="date">Текущая дата</label>
-          <input type="date" name="calendar" />
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Была ли у Вас сегодня головная боль (ГБ)?</div>
-          <input type="radio" name="headacheCheck" />
-          <label for="yes">Да</label>
-          <input type="radio" name="headacheCheck" />
-          <label for="no">Нет</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>
-            В течение часа ДО возникновения головной боли отмечати ли вы
-            зрительные нарушения (цветные вспышки, зигзаги, слепые пятна) <br />
-            или обонятельные ( ощущение посторонних запахов)
+    <form class="card" @submit.prevent="submitForm">
+      <h2>Список вопросов</h2>
+      <ol>
+        <li>
+          <div class="form-control">
+            <label for="date">Текущая дата</label>
+            <input type="date" name="calendar" />
           </div>
-          <input type="radio" name="headacheAura" />
-          <label for="yes">Да</label>
-          <input type="radio" name="headacheAura" />
-          <label for="no">Нет</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Где отмечалась головная боль?</div>
-          <input type="checkbox" name="headacheWhere" />
-          <label for="one">C одной стороны</label>
-          <input type="checkbox" name="headacheWhere" />
-          <label for="two">С обеих сторон</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Характер ГБ</div>
-          <input type="checkbox" name="headacheType" />
-          <label for="pressing">Давящая</label>
-          <input type="checkbox" name="headacheType" />
-          <label for="pulse">Пульсирующая</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>
-            Интенсивность головной боли в баллах (Выберите интенсивность из
-            списка)
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Была ли у Вас сегодня головная боль (ГБ)?</div>
+            <input type="radio" name="headacheCheck" />
+            <label for="yes">Да</label>
+            <input type="radio" name="headacheCheck" />
+            <label for="no">Нет</label>
           </div>
-
-          <select v-model="selected" class="intensity-select">
-            <option disabled value="">-- Выберите --</option>
-            <option v-for="n in 11" :key="n" :value="n - 1">{{ n - 1 }}</option>
-          </select>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Усиливалась ли при физической нагрузке?</div>
-          <input type="radio" name="headacheSport" />
-          <label for="yes">Да</label>
-          <input type="radio" name="headacheSport" />
-          <label for="no">Нет</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Сопровождалась ли тошнотой?</div>
-          <input type="radio" name="headacheNausea" />
-          <label for="yes">Да</label>
-          <input type="radio" name="headacheNausea" />
-          <label for="no">Нет</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Сопровождалась ли рвотой?</div>
-          <input type="radio" name="headachePuke" />
-          <label for="yes">Да</label>
-          <input type="radio" name="headachePuke" />
-          <label for="no">Нет</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Раздражал ли свет?</div>
-          <input type="radio" name="headacheLight" />
-          <label for="yes">Да</label>
-          <input type="radio" name="headacheLight" />
-          <label for="no">Нет</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Раздражал ли звук?</div>
-          <input type="radio" name="headacheSound" />
-          <label for="yes">Да</label>
-          <input type="radio" name="headacheSound" />
-          <label for="no">Нет</label>
-        </div>
-      </li>
-      <li>
-        <div class="form-control">
-          <div>Препарат, доза, эффект</div>
-          <input type="text" name="headacheDrug" />
-        </div>
-      </li>
-    </ol>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>
+              В течение часа ДО возникновения головной боли отмечати ли вы
+              зрительные нарушения (цветные вспышки, зигзаги, слепые пятна)
+              <br />
+              или обонятельные ( ощущение посторонних запахов)
+            </div>
+            <input type="radio" name="headacheAura" />
+            <label for="yes">Да</label>
+            <input type="radio" name="headacheAura" />
+            <label for="no">Нет</label>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Где отмечалась головная боль?</div>
+            <input type="checkbox" name="headacheWhere" />
+            <label for="one">C одной стороны</label>
+            <input type="checkbox" name="headacheWhere" />
+            <label for="two">С обеих сторон</label>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Характер ГБ</div>
+            <input type="checkbox" name="headacheType" />
+            <label for="pressing">Давящая</label>
+            <input type="checkbox" name="headacheType" />
+            <label for="pulse">Пульсирующая</label>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>
+              Интенсивность головной боли в баллах (Выберите интенсивность из
+              списка)
+            </div>
+            <select id="strong" class="intensity-select">
+              <option selected value="chois">-- Выберите --</option>
+              <option v-for="n in 11" :key="n" :value="n - 1">
+                {{ n - 1 }}
+              </option>
+            </select>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Усиливалась ли при физической нагрузке?</div>
+            <input type="radio" name="headacheSport" />
+            <label for="yes">Да</label>
+            <input type="radio" name="headacheSport" />
+            <label for="no">Нет</label>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Сопровождалась ли тошнотой?</div>
+            <input type="radio" name="headacheNausea" />
+            <label for="yes">Да</label>
+            <input type="radio" name="headacheNausea" />
+            <label for="no">Нет</label>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Сопровождалась ли рвотой?</div>
+            <input type="radio" name="headachePuke" />
+            <label for="yes">Да</label>
+            <input type="radio" name="headachePuke" />
+            <label for="no">Нет</label>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Раздражал ли свет?</div>
+            <input type="radio" name="headacheLight" />
+            <label for="yes">Да</label>
+            <input type="radio" name="headacheLight" />
+            <label for="no">Нет</label>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Раздражал ли звук?</div>
+            <input type="radio" name="headacheSound" />
+            <label for="yes">Да</label>
+            <input type="radio" name="headacheSound" />
+            <label for="no">Нет</label>
+          </div>
+        </li>
+        <li>
+          <div class="form-control">
+            <div>Препарат, доза, эффект</div>
+            <input type="text" name="headacheDrug" />
+          </div>
+        </li>
+      </ol>
+      <button type="submit" class="btn">Отправить</button>
+    </form>
   </div>
 </template>
 
