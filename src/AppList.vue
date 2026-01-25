c6d94c665dc065977dbc5682cc8d57c25704c71d
183Natasha <168049595+183Natasha@users.noreply.github.com>
2026-01-25 14:18:20 +0500

    вопросы вынесены в question.ts

--- src/AppList.vue
+++ src/AppList.vue
@@ -7,6 +7,7 @@ import AppText from "./AppText.vue";
 import AppDate from "./AppDate.vue";
 import trash from "./assets/trash.svg";
 import eye from "./assets/eye.svg";
+import * as questions from "./data/questions";
 
 const emit = defineEmits(["headache-alert", "form-deleted"]);
 
@@ -535,181 +536,181 @@ const useTest = () => {
   }
 };
 
-const basicQuestions = reactive([
-  // {
-  //   id: 1,
-  //   type: "text",
-  //   label: "Как тебя зовут?",
-  //   model: "name",
-  //   placeholder: "Введите имя",
-  //   required: true,
-  // },
-  {
-    id: 2,
-    type: "date",
-    label: "Текущая дата",
-    model: "date",
-    required: true,
-  },
-  {
-    id: 3,
-    type: "radio",
-    label: "Была ли у Вас сегодня головная боль (ГБ) или аура?",
-    model: "headacheToday",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-    required: true,
-  },
-]);
+// const basicQuestions = reactive([
+//   // {
+//   //   id: 1,
+//   //   type: "text",
+//   //   label: "Как тебя зовут?",
+//   //   model: "name",
+//   //   placeholder: "Введите имя",
+//   //   required: true,
+//   // },
+//   {
+//     id: 2,
+//     type: "date",
+//     label: "Текущая дата",
+//     model: "date",
+//     required: true,
+//   },
+//   {
+//     id: 3,
+//     type: "radio",
+//     label: "Была ли у Вас сегодня головная боль (ГБ) или аура?",
+//     model: "headacheToday",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//     required: true,
+//   },
+// ]);
 
-const headacheQuestions = reactive([
-  {
-    id: 4,
-    type: "radio",
-    label: "В течение часа ДО возникновения головной боли отмечали ли вы:",
-    sublabels: [
-      "- зрительные нарушения (цветные вспышки, зигзаги, слепые пятна)",
-      "- И/ИЛИ обонятельные (ощущение посторонних запахов)",
-    ],
-    model: "aura",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-  },
-  {
-    id: 5,
-    type: "checkbox",
-    label: "Где отмечалась головная боль?",
-    model: "location",
-    options: [
-      { value: "one", label: "C одной стороны" },
-      { value: "two", label: "С обеих сторон" },
-    ],
-  },
-  {
-    id: 6,
-    type: "checkbox",
-    label: "Характер ГБ",
-    model: "headacheType",
-    options: [
-      { value: "press", label: "Давящая" },
-      { value: "puls", label: "Пульсирующая" },
-    ],
-  },
-  {
-    id: 7,
-    type: "select",
-    label:
-      "Интенсивность головной боли в баллах (Выберите интенсивность из списка)",
-    model: "intensity",
-    options: [
-      { value: "0", label: "0 - Нет боли" },
-      { value: "1", label: "1" },
-      { value: "2", label: "2" },
-      { value: "3", label: "3" },
-      { value: "4", label: "4" },
-      { value: "5", label: "5 - Средняя боль" },
-      { value: "6", label: "6" },
-      { value: "7", label: "7" },
-      { value: "8", label: "8" },
-      { value: "9", label: "9" },
-      { value: "10", label: "10 - Невыносимая боль" },
-    ],
-    placeholder: "Выберите значение от 0 до 10", // текст по умолчанию
-  },
-  {
-    id: 8,
-    type: "radio",
-    label: "Усиливалась ли при физической нагрузке?",
-    model: "physicalActivity",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-  },
-  {
-    id: 9,
-    type: "radio",
-    label: "Сопровождалась ли тошнотой?",
-    model: "nausea",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-  },
-  {
-    id: 10,
-    type: "radio",
-    label: "Сопровождалась ли рвотой?",
-    model: "vomiting",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-  },
-  {
-    id: 11,
-    type: "radio",
-    label: "Раздражал ли свет?",
-    model: "lightSensitivity",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-  },
-  {
-    id: 12,
-    type: "radio",
-    label: "Раздражал ли звук?",
-    model: "soundSensitivity",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-  },
-  {
-    id: 13,
-    type: "radio",
-    label: "Головная боль требовала использование медикаментов",
-    model: "medication",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-  },
-]);
+// const headacheQuestions = reactive([
+//   {
+//     id: 4,
+//     type: "radio",
+//     label: "В течение часа ДО возникновения головной боли отмечали ли вы:",
+//     sublabels: [
+//       "- зрительные нарушения (цветные вспышки, зигзаги, слепые пятна)",
+//       "- И/ИЛИ обонятельные (ощущение посторонних запахов)",
+//     ],
+//     model: "aura",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//   },
+//   {
+//     id: 5,
+//     type: "checkbox",
+//     label: "Где отмечалась головная боль?",
+//     model: "location",
+//     options: [
+//       { value: "one", label: "C одной стороны" },
+//       { value: "two", label: "С обеих сторон" },
+//     ],
+//   },
+//   {
+//     id: 6,
+//     type: "checkbox",
+//     label: "Характер ГБ",
+//     model: "headacheType",
+//     options: [
+//       { value: "press", label: "Давящая" },
+//       { value: "puls", label: "Пульсирующая" },
+//     ],
+//   },
+//   {
+//     id: 7,
+//     type: "select",
+//     label:
+//       "Интенсивность головной боли в баллах (Выберите интенсивность из списка)",
+//     model: "intensity",
+//     options: [
+//       { value: "0", label: "0 - Нет боли" },
+//       { value: "1", label: "1" },
+//       { value: "2", label: "2" },
+//       { value: "3", label: "3" },
+//       { value: "4", label: "4" },
+//       { value: "5", label: "5 - Средняя боль" },
+//       { value: "6", label: "6" },
+//       { value: "7", label: "7" },
+//       { value: "8", label: "8" },
+//       { value: "9", label: "9" },
+//       { value: "10", label: "10 - Невыносимая боль" },
+//     ],
+//     placeholder: "Выберите значение от 0 до 10", // текст по умолчанию
+//   },
+//   {
+//     id: 8,
+//     type: "radio",
+//     label: "Усиливалась ли при физической нагрузке?",
+//     model: "physicalActivity",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//   },
+//   {
+//     id: 9,
+//     type: "radio",
+//     label: "Сопровождалась ли тошнотой?",
+//     model: "nausea",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//   },
+//   {
+//     id: 10,
+//     type: "radio",
+//     label: "Сопровождалась ли рвотой?",
+//     model: "vomiting",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//   },
+//   {
+//     id: 11,
+//     type: "radio",
+//     label: "Раздражал ли свет?",
+//     model: "lightSensitivity",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//   },
+//   {
+//     id: 12,
+//     type: "radio",
+//     label: "Раздражал ли звук?",
+//     model: "soundSensitivity",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//   },
+//   {
+//     id: 13,
+//     type: "radio",
+//     label: "Головная боль требовала использование медикаментов",
+//     model: "medication",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//   },
+// ]);
 
-const drugQuestions = reactive([
-  {
-    id: 14,
-    type: "text",
-    label:
-      "Заполните поля ниже, если головная боль требовала использование медикаментов",
-    sublabels: ["Какой препарат Вы приняли?"],
-    model: "drugName",
-    placeholder: "Введите название препарата",
-  },
-  {
-    id: 15,
-    type: "text",
-    label: "Какая дозировка (мг)?",
-    model: "drugMass",
-    placeholder: "Введите дозировку препарата",
-  },
-  {
-    id: 16,
-    type: "radio",
-    label: "Был эффект от приема препарата?",
-    model: "drugEffect",
-    options: [
-      { value: "yes", label: "Да" },
-      { value: "no", label: "Нет" },
-    ],
-  },
-]);
+// const drugQuestions = reactive([
+//   {
+//     id: 14,
+//     type: "text",
+//     label:
+//       "Заполните поля ниже, если головная боль требовала использование медикаментов",
+//     sublabels: ["Какой препарат Вы приняли?"],
+//     model: "drugName",
+//     placeholder: "Введите название препарата",
+//   },
+//   {
+//     id: 15,
+//     type: "text",
+//     label: "Какая дозировка (мг)?",
+//     model: "drugMass",
+//     placeholder: "Введите дозировку препарата",
+//   },
+//   {
+//     id: 16,
+//     type: "radio",
+//     label: "Был эффект от приема препарата?",
+//     model: "drugEffect",
+//     options: [
+//       { value: "yes", label: "Да" },
+//       { value: "no", label: "Нет" },
+//     ],
+//   },
+// ]);
 
 const last30Forms = computed(() => {
   if (!allForms.value || allForms.value.length === 0) {
@@ -799,7 +800,7 @@ const last30Forms = computed(() => {
       <h2>Список вопросов</h2>
       <ol>
         <!-- Вопросы 1-3 всегда показываются -->
-        <li v-for="question in basicQuestions" :key="question.id">
+        <li v-for="question in questions.basicQuestions" :key="question.id">
           <AppText
             v-if="question.type === 'text'"
             v-model="form[question.model]"
@@ -829,7 +830,7 @@ const last30Forms = computed(() => {
 
         <!-- Вопросы 4-13 показываются только если есть головная боль -->
         <div v-if="form.headacheToday === 'yes'">
-          <li v-for="question in headacheQuestions" :key="question.id">
+          <li v-for="question in questions.headacheQuestions" :key="question.id">
             <AppRadio
               v-if="question.type === 'radio'"
               v-model="form[question.model]"
@@ -862,7 +863,7 @@ const last30Forms = computed(() => {
 
         <!-- Вопросы про препараты (14-16) показываются в случае приема препаратов-->
         <div v-if="form.medication === 'yes'">
-          <li v-for="question in drugQuestions" :key="question.id">
+          <li v-for="question in questions.drugQuestions" :key="question.id">
             <AppText
               v-if="question.type === 'text'"
               v-model="form[question.model]"
